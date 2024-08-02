7067f2cec69f5b2c6f1f2e4b78789089199af672
Riadh Hajji <riadhhajji378@gmail.com>
2024-08-02 19:47:08 +0100

    commit

--- db/dbConnections.js
+++ db/dbConnections.js
@@ -1,13 +1,12 @@
 import mongoose from "mongoose";
 
 export function dbConnection() {
-  return mongoose.connect(
-    "mongodb+srv://riadhhajji:hajji90@sara7a.qcn8ipn.mongodb.net/?retryWrites=true&w=majority&appName=Sara7a"
-  )
-  .then(()=>{
-    console.log("Connected to MongoDB");
-  })
-  .catch((err)=>{
-    console.log("Error connecting to MongoDB", err);
-  })
+  return mongoose
+    .connect(process.env.DB_CONNECTION)
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+      console.log("Error connecting to MongoDB", err);
+    });
 }
--- emails/nodemailer.js
+++ emails/nodemailer.js
@@ -5,16 +5,16 @@ export const sendEMail = async (options) => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: "hajjiriadh378@gmail.com",
-      pass: "",
+      user: process.env.P_EMAIL,
+      pass: "utyzmjxxvywktqzt",
     },
   });
 
   // send mail with defined transport object
   const info = await transporter.sendMail({
-    from: '"Saraha App" <hajjiriadh378@gmail.com>', // sender address
+    from: '"Verify your sara7a account" <process.env.P_EMAIL>', // sender address
     to: options.email, // list of receivers
-    subject: "Test", // Subject line
+    subject: "Test nodemailer", // Subject line
     html: htmlCode(`http://localhost:3000/verify/${options.email}`), // html body
   });
 
